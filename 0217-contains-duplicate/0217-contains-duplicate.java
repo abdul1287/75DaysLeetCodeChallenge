5f60a4c3fd383c7bd6f6e0466f94f6e59df654c9
Abdul Ahmad <127119950+abdul1287@users.noreply.github.com>
2026-03-13 22:19:02 +0530

    Time: 16 ms (45.97%), Space: 93.5 MB (55.75%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,9 +1,21 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=1; i<nums.length; i++) {
-            if(nums[i] == nums[i-1]) {
+        // Solution 1 -> T.C -> O(nlogn) & S.C -> O(1)
+        // Arrays.sort(nums);
+        // for(int i=1; i<nums.length; i++) {
+        //     if(nums[i] == nums[i-1]) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        // Solution 2 -> T.C -> O(n) & S.C -> O(n)
+        HashSet<Integer> hs = new HashSet<>();
+        for (int num : nums) {
+            if (hs.contains(num)) {
                 return true;
+            } else {
+                hs.add(num);
             }
         }
         return false;
