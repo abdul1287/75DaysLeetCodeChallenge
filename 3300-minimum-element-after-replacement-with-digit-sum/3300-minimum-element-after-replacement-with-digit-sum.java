ee955f038c26eb41b99c310af61a8874508593c7
Abdul Ahmad <127119950+abdul1287@users.noreply.github.com>
2026-05-30 23:25:53 +0530

    Time: 1 ms (100%), Space: 44.6 MB (94.12%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
@@ -1,7 +1,8 @@
 class Solution {
     public int minElement(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = sum(String.valueOf(nums[i]));
+            // nums[i] = sum(String.valueOf(nums[i]));
+            nums[i] = sum(nums[i]);
         }
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
@@ -10,10 +11,20 @@ class Solution {
         return min;
     }
 
-    int sum(String str) {
+    // int sum(String str) {
+    //     int sum = 0;
+    //     for (int i = 0; i < str.length(); i++) {
+    //         sum += Integer.parseInt(String.valueOf(str.charAt(i)));
+    //     }
+    //     return sum;
+    // }
+
+    int sum(int num) {
         int sum = 0;
-        for (int i = 0; i < str.length(); i++) {
-            sum += Integer.parseInt(String.valueOf(str.charAt(i)));
+        while (num > 0) {
+            int rem = num % 10;
+            sum += rem;
+            num /= 10;
         }
         return sum;
     }
